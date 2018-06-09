9826ebbb996be355e1f287337efc673166e30575
Chris Xiong <chrisxiongyang@yahoo.com>
2018-06-09 16:50:06 -0700

    Added tentative title screen

--- app/src/main/java/com/chrisx/theia/MainActivity.java
+++ app/src/main/java/com/chrisx/theia/MainActivity.java
@@ -10,6 +10,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -19,6 +20,7 @@ import android.graphics.Typeface;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Handler;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Display;
 import android.view.MotionEvent;
 import android.view.WindowManager;
@@ -30,10 +32,12 @@ public class MainActivity extends Activity {
     private LinearLayout ll;
     private float scaleFactor;
 
+    static Bitmap stars;
+
     private SharedPreferences sharedPref;
     private SharedPreferences.Editor editor;
 
-    private Typeface trebuchetms;
+    private Typeface trench;
 
     private boolean paused = false;
     private long frameCount = 0;
@@ -44,9 +48,12 @@ public class MainActivity extends Activity {
     private static final int FRAMES_PER_SECOND = 60;
     private long nanosecondsPerFrame;
 
+    private int TRANSITION_MAX = FRAMES_PER_SECOND * 2 / 3;
+    private int transition = TRANSITION_MAX / 2;
+
     private float downX, downY;
 
-    private Paint title;
+    private Paint title, start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,7 +62,7 @@ public class MainActivity extends Activity {
 
         //creates the bitmap
         //note: Star 4.5 is 480x854
-        int targetH = 854,
+        int targetH = 480,
                 wpx = getAppUsableScreenSize(this).x,
                 hpx = getAppUsableScreenSize(this).y;
         scaleFactor = Math.min(1,(float)targetH/hpx);
@@ -67,6 +74,10 @@ public class MainActivity extends Activity {
         ll = (LinearLayout) findViewById(R.id.draw_area);
         ll.setBackgroundDrawable(new BitmapDrawable(bmp));
 
+        Resources res = getResources();
+        stars = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(res, R.drawable.stars),
+                Math.round(h()*2*1706/960), Math.round(h()*2), false);
+
         //initializes SharedPreferences
         sharedPref = this.getPreferences(Context.MODE_PRIVATE);
         editor = sharedPref.edit();
@@ -74,12 +85,17 @@ public class MainActivity extends Activity {
         nanosecondsPerFrame = (long)1e9 / FRAMES_PER_SECOND;
 
         //initialize fonts
-        trebuchetms = Typeface.createFromAsset(getAssets(), "fonts/TrebuchetMS.ttf");
+        trench = Typeface.createFromAsset(getAssets(), "fonts/Trench.ttf");
 
         canvas.drawColor(Color.BLACK);
 
         //pre-defined paints
         title = newPaint(Color.WHITE);
+        title.setTextAlign(Paint.Align.CENTER);
+        title.setTextSize(c480(200));
+
+        start = new Paint(title);
+        start.setTextSize(c480(50));
 
 
         final Handler handler = new Handler();
@@ -95,7 +111,12 @@ public class MainActivity extends Activity {
                     handler.post(new Runnable() {
                         @Override
                         public void run() {
-
+                            if (!paused) {
+                                //fading transition effect
+                                if (transition > 0) {
+                                    transition--;
+                                }
+                            }
                         }
                     });
 
@@ -111,8 +132,6 @@ public class MainActivity extends Activity {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                drawTitleMenu();
-
                 //draw loop
                 while (!menu.equals("quit")) {
                     final long startTime = System.nanoTime();
@@ -120,10 +139,29 @@ public class MainActivity extends Activity {
                     handler.post(new Runnable() {
                         @Override
                         public void run() {
+                            if (!paused) {
+                                if (transition < TRANSITION_MAX / 2 || menu.equals("levels")) {
+                                    if (menu.equals("start")) {
+                                        drawTitleMenu();
+                                    } else if (menu.equals("levels")) {
+                                        drawLevels();
+                                    }
+                                }
 
+                                //fading transition effect
+                                if (transition > 0 && !menu.equals("levels")) {
+                                    int t = TRANSITION_MAX / 2, alpha;
+                                    if (transition > t) {
+                                        alpha = 255 - 255 * (transition - t) / t;
+                                    } else {
+                                        alpha = 255 - 255 * (t - transition) / t;
+                                    }
+                                    canvas.drawColor(Color.argb(alpha, 20, 20, 20));
+                                }
 
-                            //update canvas
-                            ll.invalidate();
+                                //update canvas
+                                ll.invalidate();
+                            }
                         }
                     });
 
@@ -158,6 +196,16 @@ public class MainActivity extends Activity {
         float Y = event.getY()*scaleFactor;
         int action = event.getAction();
 
+        if (menu.equals("start")) {
+            if (action == MotionEvent.ACTION_DOWN) {
+                goToMenu("levels");
+            }
+        } else if (menu.equals("levels")) {
+            if (action == MotionEvent.ACTION_DOWN) {
+                goToMenu("start");
+            }
+        }
+
         return true;
     }
 
@@ -181,16 +229,16 @@ public class MainActivity extends Activity {
     private Paint newPaint(int color) {
         Paint p = new Paint(Paint.ANTI_ALIAS_FLAG);
         p.setColor(color);
-        p.setTypeface(trebuchetms);
+        p.setTypeface(trench);
 
         return p;
     }
 
     static float c480(float f) {
-        return w() / (480 / f);
+        return h() / (480 / f);
     }
     static float c854(float f) {
-        return h() / (854 / f);
+        return w() / (854 / f);
     }
 
     private long getHighScore() {
@@ -201,7 +249,28 @@ public class MainActivity extends Activity {
         return Math.PI/180*deg;
     }
 
-    private void drawTitleMenu() {
+    private void goToMenu(String s) {
+        transition = TRANSITION_MAX;
 
+        menu = s;
+    }
+
+    private void drawTitleMenu() {
+        canvas.drawBitmap(stars, w()/2-stars.getWidth()/2, 0, null);
+        canvas.drawText("THEIA", w()/2, h()/2-(title.ascent()+title.descent())/2, title);
+        int tmp = (int) (Math.abs(Math.sin(frameCount/90.*Math.PI))*255);
+        start.setAlpha(tmp);
+        canvas.drawText("tap to start", w()/2, h()/2+c480(140), start);
+    }
+
+    private void drawLevels() {
+        canvas.save();
+        canvas.translate(0, -h()*(float)(1-Math.pow(1f*transition/TRANSITION_MAX, 3)));
+
+        if (transition > 0) drawTitleMenu();
+
+
+
+        canvas.restore();
     }
 }
